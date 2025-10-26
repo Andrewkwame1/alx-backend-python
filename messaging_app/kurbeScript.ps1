12e8d572520bb4289763f88e66076ad155575f47
Andrew Acheampong <andrewacheamponh21@gmail.com>
2025-10-26 20:49:44 +0100

    k8s: update deployment image names to andrewkwame1/django-messaging-app with versioning (1.0, 1.1, 2.0)

--- messaging_app/kurbeScript.ps1
+++ messaging_app/kurbeScript.ps1
@@ -12,8 +12,8 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host "Checking for minikube..."
 if (-not (Get-Command minikube -ErrorAction SilentlyContinue)) {
-    Write-Error "minikube not found. Install minikube: https://minikube.sigs.k8s.io/docs/start/"
-    exit 1
+  Write-Error "minikube not found. Install minikube: https://minikube.sigs.k8s.io/docs/start/"
+  exit 1
 }
 
 Write-Host "Starting minikube..."
